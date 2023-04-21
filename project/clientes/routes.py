ee1c0abf3dabda44814b376289cc318224c46db3
IDGS-801-18002424 <18002424@alumnos.utleon.edu.mx>
2023-04-20 19:05:26 -0600

    de poco a poco u.u

--- project/clientes/routes.py
+++ project/clientes/routes.py
@@ -1,7 +1,7 @@
 from flask import redirect, render_template, Blueprint
 from flask import request
 from flask import url_for
-from .mainUsuarios import modificarInsertarUsuario, consultarUsuarios, consultarUsuario, eliminarUsuario
+#from .mainUsuarios import modificarInsertarUsuario, consultarUsuarios, consultarUsuario, eliminarUsuario
 from .mainReservaciones import consultarReservaciones, modificarInsertarReservacion, eliminarReservacion
 from flask import Blueprint, render_template
 from flask_security.decorators import login_required
@@ -9,34 +9,34 @@ from flask_security import login_required, current_user
 from flask_security.decorators import roles_required
 from flask import session
 
-clientes = Blueprint('clienteses_auth', __name__)
+clientes = Blueprint('clientes_auth', __name__)
 
 #Inicio usuarios
 @clientes.route("/tlachicuates/reservaciones")
 @login_required
 @roles_required('cliente')
-def vistaUsuarios():
-    resultset=consultarUsuarios()
+def vistaReservaciones():
+    resultset=consultarReservaciones()
     return render_template('/clientes/reservaciones.html', reservacion=resultset, banderaLoading=True)
 
 @clientes.route("/tlachicuates/reservaciones", methods=["POST"])
 @login_required
 @roles_required('cliente')
 def CUDvistaReservaciones():
-        idUsuario = session.get("idReservacion")
-        if int(request.form.get('bandera1'))==1:
-                modificarInsertarReservacion(idUsuario,request.form.get('txtCodigoU'),request.form.get('selectRol'),request.form.get('txtNombre'), request.form.get('txtApeMaterno'), request.form.get('txtApePaterno'),  request.form.get('txtemail'), request.form.get('txtContrasennia'), request.form.get('txtTelefono'))
-        elif int(request.form.get('bandera1'))==2:
-                eliminarReservacion(idUsuario,request.form.get('txtCodigoU'))
+    idUsuario = session.get("idUsuario")
+    if int(request.form.get('bandera1'))==1:
+        modificarInsertarReservacion(
+            int(request.form.get('txtIdReservacion')),
+            idUsuario,
+            int(request.form.get('selectMesa')),
+            int(request.form.get('selectHorario')),
+            )
+    elif int(request.form.get('bandera1'))==2:
+        eliminarReservacion(
+            int(request.form.get('txtIdReservacion')),
+            idUsuario,
+            )
            
-        return redirect(url_for('main.administradores_auth.vistaUsuarios')) 
-
-@clientes.route("/tlachicuates/reservaciones", methods=["POST", "GET"])
-@login_required
-@roles_required('cliente')
-def vistaReservaciones():
-    resultset=consultarReservaciones()
-    reservacion=consultarReservaciones(request.form.get('bandera'))
-    return render_template('/clientes/reservaciones.html', reservacion=reservacion, reservacion=resultset)
+    return redirect(url_for('main.clientes_auth.vistaReservaciones')) 
 
 #Fin usuarios
